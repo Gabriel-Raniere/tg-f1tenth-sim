7b0fae2e3eed2116b4e643172b243cde2f000e5a
Gabriel Raniere <gabriel.raniere@tenda.digital>
2025-03-13 01:29:20 -0300

    fix emergency brakes

--- packages/safety/safety/emergency_brake.py
+++ packages/safety/safety/emergency_brake.py
@@ -5,21 +5,26 @@ from sensor_msgs.msg import LaserScan
 from nav_msgs.msg import Odometry
 from ackermann_msgs.msg import AckermannDriveStamped
 import numpy as np
+import time
 
 
 class EmergencyBrake(Node):
   #car specs
-  maxDeceleration: float = 1 #m/s^2
-  safetyRadius: float = 0.3 #m
+  max_deceleration: float = 0.7 #m/s^2
+  car_width: float = 0.2 #m
+  speed_cutoff: float = 0.1 #m/s
+  max_speed: float = 0.0 #m/s
+  safety_distance: float = 1.0 #m
   
   #class properties
   ttc = []
   
   #message variables
   last_odom: Odometry = None
+  last_scan: LaserScan = None
   
   #helpers
-  callCount: int = 0
+  previous_time: float = None
 
 
   def __init__(self):
@@ -47,6 +52,7 @@ class EmergencyBrake(Node):
 
 
   def scan_callback(self, msg: LaserScan):
+    print(self.max_speed)
     self.calculate_ttc(msg)
     self.break_if_needed()
     self.last_scan = msg
@@ -54,7 +60,7 @@ class EmergencyBrake(Node):
   def odom_callback(self, msg):
     self.last_odom = msg
 
-  def calculate_ttc(self, curr_scan):
+  def calculate_ttc_old(self, curr_scan):
     if self.last_odom == None:
       return
     
@@ -74,14 +80,56 @@ class EmergencyBrake(Node):
     self.callCount += 1
     if self.callCount % 10 == 0:
       self.get_logger().info(str(ttc[middle_index]))
+  
+  def calculate_ttc(self, curr_scan):
+    if self.last_odom == None:
+      return
+    if self.last_scan == None:
+      return
+    
+    curr_time = time.time()
+    speeds = []
+    ttcs = []
+    max_speeds = []
+    
+    i = 0
+    speed_x = self.last_odom.twist.twist.linear.x or 1e-10
+    while i < len(self.last_scan.ranges):
+      curr_range = curr_scan.ranges[i]
+      angle = i * curr_scan.angle_increment + curr_scan.angle_min
       
+      # calculate ttc
+      speed = speed_x * np.cos(angle)
+      speeds.append(speed)
+
+      #calculate max speeds for car front
+      i += 1
+      if np.abs(np.sin(angle) * curr_range) < self.car_width/2:
+        ttc =  (curr_range - self.safety_distance)/speed
+        ttcs.append(ttc)
+        max_speed = max(self.max_deceleration * ttc, 0)
+        max_speeds.append(max_speed)
+      else:
+        continue
+    
+    min_max_speed = min(max_speeds)
+    if min_max_speed < self.speed_cutoff:
+      self.max_speed = 0.0
+    else:
+      self.max_speed = min_max_speed
+  
   def break_if_needed(self):
-    MIN_TTC = 2 # ttc in seconds
-    for ttc in self.ttc:
-      if ttc < MIN_TTC:
-        ackerman_msg = AckermannDriveStamped()
-        ackerman_msg.drive.speed = 0.0
-        self.ackerman_publisher.publish(ackerman_msg)
+    if self.last_odom == None:
+      return
+    ackerman_msg = AckermannDriveStamped()
+    ackerman_msg.drive.speed = self.max_speed
+    self.ackerman_publisher.publish(ackerman_msg)
+    # MIN_TTC = 2 # ttc in seconds
+    # for ttc in self.ttc:
+    #   if ttc < MIN_TTC:
+    #     ackerman_msg = AckermannDriveStamped()
+    #     ackerman_msg.drive.speed = 0.0
+    #     self.ackerman_publisher.publish(ackerman_msg)
 
 
 def main(args=None):
